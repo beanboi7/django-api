9832766f5021e3ca199ee2046d7c367b183d3f8f
beanboi7 <srirocksplayer@gmail.com>
2021-05-06 23:02:09 +0530

    ok

--- user/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import AddUser
-
-admin.site.register(AddUser)
--- user/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import include,path
-from django.conf.urls import url
-from .api import RegisterApi
-
-urlpatterns = [
-    path('register/', RegisterApi.as_view()),
-]
